92abdab1c995c9f624b0d16ceaceb1da325973b5
tangguo <545496535@qq.com>
2021-08-08 02:06:04 +0800

    date 2021/8/8 leetcode simple commit

--- src/ParkingSystem.java
+++ src/ParkingSystem.java
@@ -49,8 +49,8 @@ class ParkingSystem {
     }
 
     public static void main(String[] args) throws Exception {
-        int n = 1;
-        System.out.println(new ParkingSystem().isThree(n));
+        int n = 5;
+        System.out.println(new ParkingSystem().tribonacci(n));
     }
 
     public int maxDepth(String s) {
@@ -1931,6 +1931,27 @@ class ParkingSystem {
             }
         }
         return new String(result);
+    }
+
+    // 1137. 第 N 个泰波那契数
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i-3] + arr[i-2] + arr[i-1];
+        }
+        return arr[arr.length-1];
     }
 }
 
