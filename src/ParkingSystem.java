c5bfdecbd04683a6721d2dd44d306d726601a390
candy-yyyyy <545496535@qq.com>
2021-10-16 23:08:07 +0800

    leetcode

--- src/ParkingSystem.java
+++ src/ParkingSystem.java
@@ -2205,6 +2205,18 @@ class ParkingSystem {
         }
         return 1;
     }
+
+    // 5885. 使每位学生都有座位的最少移动次数
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int num = 0;
+        for (int i = 0; i < seats.length; i++) {
+            num += Math.abs(seats[i] - students[i]);
+        }
+        return num;
+    }
+
 }
 
 /**
