87022c24b2f59c22a1a3e74196d9652a84ab4025
candy-yyyyy <545496535@qq.com>
2021-10-31 11:26:45 +0800

    leetcode

--- src/ParkingSystem.java
+++ src/ParkingSystem.java
@@ -2257,6 +2257,16 @@ class ParkingSystem {
         }
         return "";
     }
+
+    // 5914. 值相等的最小索引
+    public int smallestEqual(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 10  == nums[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
 /**
