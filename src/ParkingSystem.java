dc18bc0bd19be91b2838c0c362b7c146f05672ab
c7n6y <545496535@qq.com>
2022-09-03 22:55:02 +0800

    default commit

--- src/ParkingSystem.java
+++ src/ParkingSystem.java
@@ -3576,6 +3576,20 @@ class ParkingSystem {
         }
         return count;
     }
+
+    // 6171. 和相等的子数组
+    public boolean findSubarrays(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            int sum = nums[i] + nums[i + 1];
+            if (set.contains(sum)) {
+                return true;
+            } else {
+                set.add(sum);
+            }
+        }
+        return false;
+    }
 }
 
 /**
